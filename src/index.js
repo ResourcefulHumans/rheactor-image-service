1c140520939894fcecad3dcdad0b8d1be66866e8
Markus Tacker <m@coderbyheart.com>
2017-02-24 18:52:13 +0100

    fix(production): the key identifier needs to be different on lambda

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ const contentType = 'application/vnd.resourceful-humans.rheactor-image-service.v
 const version = process.env.VERSION
 const environment = process.env.NODE_ENV
 const deployTime = process.env.DEPLOY_TIME
-const publicKey = `-----BEGIN PUBLIC KEY-----\n${process.env.PUBLIC_KEY.match(/.{1,64}/g).join('\n')}\n-----END PUBLIC KEY-----`
+// TODO: Load the key public from S3
+const keyType = environment === 'testing' ? 'PUBLIC KEY' : 'RSA PUBLIC KEY'
+const publicKey = `-----BEGIN ${keyType}-----\n${process.env.PUBLIC_KEY.match(/.{1,64}/g).join('\n')}\n-----END ${keyType}-----`
 
 const s3 = new AWS.S3({
   region: process.env.S3_REGION,
